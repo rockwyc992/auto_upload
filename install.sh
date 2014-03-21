4b94238279595342f886d46d17a729932fbed69c
rockwyc992 <rockwyc992@gmail.com>
2014-03-21 02:45:36 +0000

    install finish

--- install.sh
+++ install.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-sudo cp zip.ce1002 /usr/bin/
-sudo cp vim.ce1002 /usr/bin/
-sudo cp upload.ce1002 /usr/bin/
+cp zip.ce1002 "$HOME"/.au_ce1002/bin/
+cp vim.ce1002 "$HOME"/.au_ce1002/bin/
+cp upload.ce1002 "$HOME"/.au_ce1002/bin/
 cp auto_upload.ce1002.vim "$HOME"/.vim/plugin/
 
 read -p '請輸入lms帳號：' user
 read -p '請輸入lms密碼：' passwd
 
-echo 'user='"$user" > "$HOME/.config/auto_upload.ce1002"
-echo 'password='"$passwd" >> "$HOME/.config/auto_upload.ce1002"
+echo 'user='"$user" > "$HOME/.au_ce1002/config"
+echo 'password='"$passwd" >> "$HOME/.au_ce1002/config"
 
+echo '' >> "$HOME"/.bash_profile
+echo '[[ $PATH =~ "$HOME/.au_ce1002/bin" ]] || PATH="$PATH":"$HOME"/.au_ce1002/bin' >> "$HOME"/.bash_profile
+echo '' >> "$HOME"/.bashrc
+echo '[[ $PATH =~ "$HOME/.au_ce1002/bin" ]] || PATH="$PATH":"$HOME"/.au_ce1002/bin' >> "$HOME"/.bashrc
